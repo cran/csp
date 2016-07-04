8bcfb76c55d8cc82dfdc9a9012631dc64c0ec643
Eric Persson <expersso5@gmail.com>
2016-07-04 20:29:15 +0000

    version 0.1.0

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Correlates of State Policy Dataset
+#'
+#' This package contains the Correlates of State Policy dataset, which includes
+#' more than seven-hundred variables, with observations across the U.S. 50
+#' states and time (1900 - 2016).
+#'
+#' For more information, go to the
+#' \href{http://ippsr.msu.edu/public-policy/correlates-state-policy}{official website}.
+#' @examples
+#' library(csp)
+#' data(csp, package = "csp")
+#' dim(csp)
+#' names(csp)
+#' csp[1:2, ]
+"csp"
